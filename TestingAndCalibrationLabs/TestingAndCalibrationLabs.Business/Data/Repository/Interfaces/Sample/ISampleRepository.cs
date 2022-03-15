2db68d4c08151e3c0fdae34d77ad618f28f745e3
Shreenidhi H D <shreenidhihaagalagodu@gmail.com>
2022-03-15 15:43:24 +0530

    Updated all
    Added comment

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.Business/Data/Repository/Interfaces/Sample/ISampleRepository.cs
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Business/Data/Repository/Interfaces/Sample/ISampleRepository.cs
@@ -9,11 +9,41 @@ namespace TestingAndCalibrationLabs.Business.Data.Repository.Interfaces
     /// </summary>
     public interface ISampleRepository
     {
+        ///created Instert interface
         int Insert(SampleModel sample);
+
+        /// <summary>
+        /// created interface for update
+        /// </summary>
+        /// <param name="sample"></param>
+        /// <returns></returns>
         int Update(SampleModel sample);
+
+        /// <summary>
+        /// created interface for listing
+        /// </summary>
+        /// <returns></returns>
         List<SampleModel> Get();
+
+        /// <summary>
+        /// created interface for get
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         SampleModel Get(int id);
+
+        /// <summary>
+        /// crearte interface for delete
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         bool Delete(int id);
+        
+        /// <summary>
+        /// create interface for insert collection
+        /// </summary>
+        /// <param name="sample"></param>
+        /// <returns></returns>
         int InsertCollection(List<SampleModel> sample);
     }
 }
