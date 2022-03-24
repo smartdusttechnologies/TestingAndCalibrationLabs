1062d658cbfc992bfafc12d5c9d34e7389ef3758
Sumanth M S <sumanthkalasa@gmail.com>
2022-03-24 19:35:53 +0530

    Sign-In , Sign-Up and Nav-bar
    Sign-In and Sign-Up Thing is done properly
    In nav-bar collapse thing needed to be alter properly

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Startup.cs
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Startup.cs
@@ -58,7 +58,7 @@ namespace TestingAndCalibrationLabs.Web.UI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=login}/{id?}");
             });
         }
     }
