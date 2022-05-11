f519ec4eb42ddfe136cc92d09772332ee44a804e
Shreenidhi H D <shreenidhihaagalagodu@gmail.com>
2022-05-11 20:08:29 +0530

    updated query

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/Scripts/TestingCategoryLookupDataInsertion.sql
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/Scripts/TestingCategoryLookupDataInsertion.sql
@@ -1,5 +1,5 @@
-insert into TestingCategoryLookup1(Name,Descrpition,Position,IsDeleted) values ('SOMEONE','shreesmartdusttech','1','0');
+insert into TestingCategoryLookup(Name,Descrpition,Position,IsDeleted) values ('SOMEONE','shreesmartdusttech','1','0');
 
-insert into TestingCategoryLookup1(Name,Descrpition,ParentId,Position,IsDeleted) values ('SOMEONE2','shreesmartdusttech',(Select SCOPE_IDENTITY()),'1','0');
+insert into TestingCategoryLookup(Name,Descrpition,ParentId,Position,IsDeleted) values ('SOMEONE2','shreesmartdusttech',(Select SCOPE_IDENTITY()),'1','0');
 
-insert into TestingCategoryLookup1(Name,Descrpition,ParentId,Position,IsDeleted) values ('SOMEONE3','shreesmartdusttech',(Select SCOPE_IDENTITY()),'1','0');
+insert into TestingCategoryLookup(Name,Descrpition,ParentId,Position,IsDeleted) values ('SOMEONE3','shreesmartdusttech',(Select SCOPE_IDENTITY()),'1','0');
