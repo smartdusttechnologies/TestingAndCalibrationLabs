b96742702bbed80f81c773ca4de5517efd9f12a6
Sumanth M S <sumanthkalasa@gmail.com>
2022-06-06 19:32:02 +0530

    TestDetails
    Some of the input feilds were there in the Test Details....Those were done

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Controllers/HomeController.cs
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Controllers/HomeController.cs
@@ -29,5 +29,13 @@ namespace TestingAndCalibrationLabs.Web.UI.Controllers
             return View();
         }
 
+        /// <summary>
+        /// UI Shows the Various Plans and respective Prices
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult TestDetails()
+        {
+            return View();
+        }
     }
 }
