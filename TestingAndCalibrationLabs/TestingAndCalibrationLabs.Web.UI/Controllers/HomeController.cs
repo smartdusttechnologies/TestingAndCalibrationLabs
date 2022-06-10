fc5fce9dfeaab9d3fe5a29db3dd0516b1b1021e8
Sumanth M S <sumanthkalasa@gmail.com>
2022-06-10 16:06:37 +0530

    Grid
    It consists of the Grid UI and the data for the Grid is coming from the Controller

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Controllers/HomeController.cs
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestingAndCalibrationLabs.Web.UI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TestingAndCalibrationLabs.Web.UI.Controllers
@@ -30,12 +31,47 @@ namespace TestingAndCalibrationLabs.Web.UI.Controllers
         }
 
         /// <summary>
-        /// UI Shows the Various Plans and respective Prices
+        /// UI will get the information from the User
         /// </summary>
         /// <returns></returns>
         public IActionResult TestDetails()
         {
-            return View();
+            //here hardcoding the values instread of accessing from the DB
+            List<TestDetailsModel> TestDetailsOfUsers = new List<TestDetailsModel>();
+            for(var i = 0; i < 5; i++)
+            {
+                var tempDetails=new TestDetailsModel();
+                tempDetails.SampleType = "Testing Tools";
+                tempDetails.SampleDetails = "NA";
+                tempDetails.DateOfTP= DateTime.Now;
+                tempDetails.JobCode = 1122;
+                tempDetails.SampleId = 5566;
+                tempDetails.NumberOfSampleQuantity = 10;
+                tempDetails.TestName = "Testing Meterial";
+                tempDetails.TestMethod = "Meterial Strenght";
+                tempDetails.PersonAuthorized = "Sumanth";
+                tempDetails.ReceivedOn= DateTime.Now;
+                tempDetails.TargedOn = DateTime.Now;
+                tempDetails.CompletedOn= DateTime.Now;
+                tempDetails.Remarks = "Some Comments";
+                TestDetailsOfUsers.Add(tempDetails);
+                var tempDetailss = new TestDetailsModel();
+                tempDetailss.SampleType = "Testing Meterials";
+                tempDetailss.SampleDetails = "NA";
+                tempDetailss.DateOfTP = DateTime.Now;
+                tempDetailss.JobCode = 2299;
+                tempDetailss.SampleId = 1155;
+                tempDetailss.NumberOfSampleQuantity = 30;
+                tempDetailss.TestName = "Testing Product";
+                tempDetailss.TestMethod = "Meterial Quality";
+                tempDetailss.PersonAuthorized = "Rishi";
+                tempDetailss.ReceivedOn = DateTime.Now;
+                tempDetailss.TargedOn = DateTime.Now;
+                tempDetailss.CompletedOn = DateTime.Now;
+                tempDetailss.Remarks = "Some Guidance";
+                TestDetailsOfUsers.Add(tempDetailss);
+            }
+            return View(TestDetailsOfUsers);
         }
     }
 }
---
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Models/TestDetailsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestingAndCalibrationLabs.Web.UI.Models
+{
+    public class TestDetailsModel
+    {
+        public string SampleType { get; set; }
+        public string SampleDetails { get; set; }
+         public DateTime DateOfTP   { get; set; }
+        public int JobCode { get; set; }
+        public int SampleId { get; set; }
+        public int NumberOfSampleQuantity { get; set; }
+        public string TestName { get; set; }
+        public string TestMethod { get; set; }
+        public string PersonAuthorized { get; set; }
+        public DateTime ReceivedOn { get; set; }
+        public DateTime TargedOn { get; set; }
+        public DateTime CompletedOn { get; set; }
+        public string Remarks { get; set; }
+    }
+}
