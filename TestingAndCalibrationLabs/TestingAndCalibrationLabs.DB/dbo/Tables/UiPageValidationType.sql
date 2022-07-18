466b4a62773d85aab5abb59b0545d58d0e899ab9
Shreenidhi H D <shreenidhihaagalagodu@gmail.com>
2022-07-18 17:21:22 +0530

    updated script

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/UiPageValidationType.sql
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/UiPageValidationType.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[UiPageValidationType] (
     [Id]        INT           IDENTITY (1, 1) NOT NULL,
     [Name]      VARCHAR (100) NOT NULL,
+	[Message] varchar(100) NOT NULL,
+	[Value] varchar(100) NOT NULL,
     [IsDeleted] BIT           CONSTRAINT [DF_UiPageValidationType_IsDeleted] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_UiPageValidationType] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
