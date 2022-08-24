9e4b74c24ab88a15a5f6b636cadae4f919a39d31
rajguptaH <rajkumar00999.rk@gmail.com>
2022-08-24 10:59:29 +0530

    Id Is Set To Identity(1,1)

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/UiPageValidation.sql
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/UiPageValidation.sql
@@ -1,14 +1,16 @@
 ﻿CREATE TABLE [dbo].[UiPageValidation] (
-    [Id]                     INT NOT NULL,
+    [Id]                     INT IDENTITY (1, 1) NOT NULL,
     [UiPageTypeId]           INT NOT NULL,
     [UiPageMetadataId]       INT NOT NULL,
     [UiPageValidationTypeId] INT NOT NULL,
-    [IsDeleted]              BIT CONSTRAINT [DF_UiPageValidation_IsDeleted] DEFAULT ((0)) NOT NULL,
-    CONSTRAINT [PK_UiPageValidation] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_UiPageValidation_UiPageMetadata_Id] FOREIGN KEY ([UiPageMetadataId]) REFERENCES [dbo].[UiPageMetadata] ([Id]),
-    CONSTRAINT [FK_UiPageValidation_UiPageType_Id] FOREIGN KEY ([UiPageTypeId]) REFERENCES [dbo].[UiPageType] ([Id]),
-    CONSTRAINT [FK_UiPageValidation_UiPageValidationType_Id] FOREIGN KEY ([UiPageValidationTypeId]) REFERENCES [dbo].[UiPageValidationType] ([Id])
+    [IsDeleted]              BIT DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_UiPageValidation_1] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UiPageValidation_UiPageMetadataId] FOREIGN KEY ([UiPageMetadataId]) REFERENCES [dbo].[UiPageMetadata] ([Id]),
+    CONSTRAINT [FK_UiPageValidation_UiPageTypeId] FOREIGN KEY ([UiPageTypeId]) REFERENCES [dbo].[UiPageType] ([Id]),
+    CONSTRAINT [FK_UiPageValidation_UiPageValidationTypeId] FOREIGN KEY ([UiPageValidationTypeId]) REFERENCES [dbo].[UiPageValidationType] ([Id])
 );
+
+
 
 
 
