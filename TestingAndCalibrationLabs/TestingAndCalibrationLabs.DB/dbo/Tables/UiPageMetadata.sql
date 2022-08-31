9f342b89e6f138e5fc86a00ef02035fab1f9bb53
rajguptaH <rajkumar00999.rk@gmail.com>
2022-08-31 15:14:44 +0530

    Deleted FIles ,Proper Naming

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/UiPageMetadata.sql
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/UiPageMetadata.sql
@@ -7,9 +7,9 @@
     [IsDeleted]            BIT           CONSTRAINT [DF_UiPageMetadata_IsDeleted] DEFAULT ((0)) NOT NULL,
     [DataTypeId]           BIGINT        NOT NULL,
     CONSTRAINT [PK_UiPageMetadata] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_UiPageMetadata_DataTypeId] FOREIGN KEY ([DataTypeId]) REFERENCES [dbo].[DataType] ([Id]),
-    CONSTRAINT [FK_UiPageMetadata_UiControlTypeId] FOREIGN KEY ([UiControlTypeId]) REFERENCES [dbo].[UiControlType] ([id]),
-    CONSTRAINT [FK_UiPageMetadata_UiPageTypeId] FOREIGN KEY ([UiPageTypeId]) REFERENCES [dbo].[UiPageType] ([Id])
+    CONSTRAINT [FK_UiPageMetadata_DataType_Id] FOREIGN KEY ([DataTypeId]) REFERENCES [dbo].[DataType] ([Id]),
+    CONSTRAINT [FK_UiPageMetadata_UiControlType_Id] FOREIGN KEY ([UiControlTypeId]) REFERENCES [dbo].[UiControlType] ([id]),
+    CONSTRAINT [FK_UiPageMetadata_UiPageType_Id] FOREIGN KEY ([UiPageTypeId]) REFERENCES [dbo].[UiPageType] ([Id])
 );
 
 
--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/UiPageValidation.sql
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/UiPageValidation.sql
@@ -5,9 +5,9 @@
     [UiPageValidationTypeId] INT NOT NULL,
     [IsDeleted]              BIT DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_UiPageValidation] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_UiPageValidation_UiPageMetadataId] FOREIGN KEY ([UiPageMetadataId]) REFERENCES [dbo].[UiPageMetadata] ([Id]),
-    CONSTRAINT [FK_UiPageValidation_UiPageTypeId] FOREIGN KEY ([UiPageTypeId]) REFERENCES [dbo].[UiPageType] ([Id]),
-    CONSTRAINT [FK_UiPageValidation_UiPageValidationTypeId] FOREIGN KEY ([UiPageValidationTypeId]) REFERENCES [dbo].[UiPageValidationType] ([Id])
+    CONSTRAINT [FK_UiPageValidation_UiPageMetadata_Id] FOREIGN KEY ([UiPageMetadataId]) REFERENCES [dbo].[UiPageMetadata] ([Id]),
+    CONSTRAINT [FK_UiPageValidation_UiPageType_Id] FOREIGN KEY ([UiPageTypeId]) REFERENCES [dbo].[UiPageType] ([Id]),
+    CONSTRAINT [FK_UiPageValidation_UiPageValidationType_Id] FOREIGN KEY ([UiPageValidationTypeId]) REFERENCES [dbo].[UiPageValidationType] ([Id])
 );
 
 
