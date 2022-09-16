6026c941993a8c0b1702fb3232494cae797dcbad
sauravgclys890 <48451233+sauravgclys890@users.noreply.github.com>
2022-09-16 21:16:15 +0530

    Adding the correct sql formatted scripts

---
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/ClaimType.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ClaimType] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      VARCHAR (100) NOT NULL,
+    [IsDeleted] BIT           CONSTRAINT [DF_ClaimType_IsDeleted] DEFAULT ((0)) NOT NULL,
+    [Value]     VARCHAR (250) NOT NULL,
+    CONSTRAINT [PK_ClaimType] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/UserRoleClaim.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[UserRoleClaim] (
+    [Id]           INT    IDENTITY (1, 1) NOT NULL,
+    [RoleId]       BIGINT NOT NULL,
+    [PermissionId] INT    NOT NULL,
+    [ClaimTypeId]  INT    NOT NULL,
+    [IsDeleted]    BIT    CONSTRAINT [DF_UserRoleClaim_IsDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_UserRoleClaim] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserRoleClaim_ClaimType_Id] FOREIGN KEY ([ClaimTypeId]) REFERENCES [dbo].[ClaimType] ([Id]),
+    CONSTRAINT [FK_UserRoleClaim_Permission_Id] FOREIGN KEY ([PermissionId]) REFERENCES [dbo].[Permission] ([Id]),
+    CONSTRAINT [FK_UserRoleClaim_Role_Id] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[Role] ([Id])
+);
+
