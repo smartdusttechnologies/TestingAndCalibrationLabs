3b37aaf6e7b5902e936950e5b48d68b4f70ca423
ritesh52n <111060494+ritesh52n@users.noreply.github.com>
2022-10-26 20:13:48 +0530

    Updated

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/wwwroot/js/Notification.js
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/wwwroot/js/Notification.js
@@ -1,60 +1,53 @@
 ﻿//variables to manipulate bell Icon Content
 
 var count = 1;
-var i = 0;
-var mess = " ";
-const z = [];
+var NotificationNumbers = 0;
+var message = " ";
+const NotificationItem = [];
 //To hide or display the BellIcon
 function DisplayNotification() {
 
-    var x = document.getElementById("Menu");
+    var Icon = document.getElementById("Menu");
     var IconRotate = document.getElementById("rotate");
-    if (x.style.display == "none") {
-        
-        IconRotate.classList.toggle("down");
-        x.style.display = "block";
+    if (Icon.style.display == "none") {
 
-    } else {
         IconRotate.classList.toggle("down");
-        x.style.display = "none";
-        for (var item in z) {
-            z[item].style = "background-color:white";
+        Icon.style.display = "block";
+    }
+    else {
+        IconRotate.classList.toggle("down");
+        Icon.style.display = "none";
+        for (var item in NotificationItem) {
+            NotificationItem[item].style = "background-color:white";
         }
-        
     }
 };
 
 // To Put the Notification content in the Bell Icon
-function ErrorMessage(value,mess) {
-  
-    if (value ==0) {
+function ErrorMessage(value, message) {
+
+    if (value == 0) {
         var Notify = document.getElementById("value");
         Notify.innerHTML = "0";
         count = 0;
         document.getElementById("content").innerHTML = " ";
-        
+
     }
-    
-        count++;
+
+    count++;
     $('#content').prepend("<div class='notification-list' id='Paragraph'></div>");
-
     $('#Paragraph').append("<div class='notification-list_detail' id='child'></div>");
-
-    $('#child').append(" <b><p id='Name'  > " + mess + "</p></b>");
-
+    $('#child').append(" <b><p id='Name'  > " + message + "</p></b>");
     var Notify = document.getElementById("value");
-
     Notify.innerHTML = count;
-    z[i] = document.getElementById('Paragraph');
-    z[i].style = "background-color: #E9E9E9;";
+    NotificationItem[NotificationNumbers] = document.getElementById('Paragraph');
+    NotificationItem[NotificationNumbers].style = "background-color: #E9E9E9;";
     var View = document.getElementById("view");
-    View.style.display = "block";  
-    z[i].id = "Paragraph" + i;
-   
- 
+    View.style.display = "block";
+    NotificationItem[NotificationNumbers].id = "Paragraph" + NotificationNumbers;
 };
 
-function SuccessMessage(value, mess) {
+function SuccessMessage(value, message) {
 
     if (value == 0) {
         var Notify = document.getElementById("value");
@@ -66,34 +59,30 @@ function SuccessMessage(value, mess) {
     count++;
     $('#content').prepend("<div class='notification-list' id='Paragraph'></div>");
     $('#Paragraph').append("<div class='notification-list_detail' id='child'></div>");
-      $('#child').append(" <b><p id='Name' style='color:green' > " + mess + "</p></b>");
+    $('#child').append(" <b><p id='Name' style='color:green' > " + message + "</p></b>");
     var Notify = document.getElementById("value");
     Notify.innerHTML = count;
-
-    z[i] = document.getElementById('Paragraph');
-    z[i].style = "background-color: #E9E9E9;";
-
-    
+    NotificationItem[NotificationNumbers] = document.getElementById('Paragraph');
+    NotificationItem[NotificationNumbers].style = "background-color: #E9E9E9;";
     var View = document.getElementById("view");
     View.style.display = "block";
-
 };
 
- //To delete the Notification
+//To delete the Notification
 function Delete() {
     var View = document.getElementById("view");
     View.style.display = "none";
     var Notify = document.getElementById("value");
     Notify.innerHTML = "0";
     count = 0;
-    i = 0;
+    NotificationNumbers = 0;
     document.getElementById("content").innerHTML = "<span style='font-weight:bold;'><center>No Notification </center></span>";
 };
 
 //To close the BellIcon on Outside Click
 window.onclick = function () {
-    var x = document.getElementById("Menu");
-    if (x.style.display == "block") {
+    var Icon = document.getElementById("Menu");
+    if (Icon.style.display == "block") {
         DisplayNotification();
     }
 }
