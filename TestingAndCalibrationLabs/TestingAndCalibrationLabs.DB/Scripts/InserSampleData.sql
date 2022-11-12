e47d090baedbe2fd997a9dd09f5d345d5985e897
rajguptaH <rajkumar00999.rk@gmail.com>
2022-11-12 14:21:06 +0530

    Extra Seed Data Deleted

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/Scripts/InserSampleData.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO SampleTable(Name,Description,IsDeleted) values ('EyeBrow','EyeBrow','0')
-INSERT INTO SampleTable(Name,Description,IsDeleted) values ('Eye','Eye','0')
-INSERT INTO SampleTable(Name,Description,IsDeleted) values ('india','aaa','0')
