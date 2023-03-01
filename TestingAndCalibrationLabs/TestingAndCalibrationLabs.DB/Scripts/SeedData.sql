9ab3bb0c5518797ffd47791113cdb2636565d8fb
AMAN <amankumara527@gmail.com>
2023-03-01 10:50:26 +0530

    crud
    crud

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/Scripts/SeedData.sql
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/Scripts/SeedData.sql
@@ -376,7 +376,7 @@ BEGIN
     SET IDENTITY_INSERT [dbo].[UiNavigationCategory]  OFF
 END
 GO
-IF NOT EXISTS (SELECT 1 FROM [UiPageNavigation] WHERE Id IN (0,1,2,3,4,5,6,7,8,9,10,11))
+IF NOT EXISTS (SELECT 1 FROM [UiPageNavigation] WHERE Id IN (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15))
 BEGIN
     SET IDENTITY_INSERT [dbo].[UiPageNavigation]  ON
 
