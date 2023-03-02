830a54df41153e42a7082584e12e0d006b4a1785
AMAN <amankumara527@gmail.com>
2023-03-02 07:45:34 +0530

    crud
    crud

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Controllers/ModuleController.cs
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Controllers/ModuleController.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using TestingAndCalibrationLabs.Business.Core.Interfaces;
 using TestingAndCalibrationLabs.Business.Core.Model;
-using TestingAndCalibrationLabs.Business.Services;
 using TestingAndCalibrationLabs.Web.UI.Models;
 
 
--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Controllers/WorkflowActivityController.cs
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Controllers/WorkflowActivityController.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using TestingAndCalibrationLabs.Business.Core.Interfaces;
 using TestingAndCalibrationLabs.Business.Core.Model;
-using TestingAndCalibrationLabs.Business.Data.Repository.Interfaces;
-using TestingAndCalibrationLabs.Business.Services;
 using TestingAndCalibrationLabs.Web.UI.Models;
 
 namespace TestingAndCalibrationLabs.Web.UI.Controllers
