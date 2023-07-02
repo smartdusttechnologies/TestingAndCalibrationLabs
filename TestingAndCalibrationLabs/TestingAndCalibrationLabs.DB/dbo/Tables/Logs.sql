41e821e275e12eb3c782b71f59de3deb2a1a661a
AMAN <amankumara527@gmail.com>
2023-07-02 10:24:27 +0530

    logger implementation

---
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/Logs.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Logs] (
+    [ID]          INT            IDENTITY (1, 1) NOT NULL,
+    [MachineName] NVARCHAR (200) NULL,
+    [Logged]      DATETIME       NOT NULL,
+    [Level]       VARCHAR (5)    NOT NULL,
+    [Message]     NVARCHAR (MAX) NOT NULL,
+    [Logger]      NVARCHAR (300) NULL,
+    [Properties]  NVARCHAR (MAX) NULL,
+    [Callsite]    NVARCHAR (300) NULL,
+    [Exception]   NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.Logs] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/usp_Logs_AddEntry.sql
@@ -0,0 +1,31 @@
+﻿Create PROCEDURE [dbo].[usp_Logs_AddEntry] (
+  @machineName nvarchar(200),
+  @logged datetime,
+  @level varchar(5),
+  @message nvarchar(max),
+  @logger nvarchar(300),
+  @properties nvarchar(max),
+  @callsite nvarchar(300),
+  @exception nvarchar(max)
+) AS
+BEGIN
+  INSERT INTO [dbo].[Logs] (
+    [MachineName],
+    [Logged],
+    [Level],
+    [Message],
+    [Logger],
+    [Properties],
+    [Callsite],
+    [Exception]
+  ) VALUES (
+    @machineName,
+    @logged,
+    @level,
+    @message,
+    @logger,
+    @properties,
+    @callsite,
+    @exception
+  );
+END
