828c84b0966f876e95b039351c7592f202582a71
rajguptaH <rajkumar00999.rk@gmail.com>
2023-08-23 00:28:36 +0530

    FK Id formated

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/Permission.sql
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/Permission.sql
@@ -6,7 +6,7 @@
     [IsDeleted]              BIT           CONSTRAINT [DF_Permission_isDeleted] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_Permission] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Permission_PermissionType_Id] FOREIGN KEY ([PermissionTypeId]) REFERENCES [dbo].[PermissionType] ([Id]),
-    CONSTRAINT [FK_Permission_SubPermissionModuleId] FOREIGN KEY ([PermissionModuleTypeId]) REFERENCES [dbo].[SubPermissionModuleType] ([Id])
+    CONSTRAINT [FK_Permission_SubPermissionModule_Id] FOREIGN KEY ([PermissionModuleTypeId]) REFERENCES [dbo].[SubPermissionModuleType] ([Id])
 );
 
 
