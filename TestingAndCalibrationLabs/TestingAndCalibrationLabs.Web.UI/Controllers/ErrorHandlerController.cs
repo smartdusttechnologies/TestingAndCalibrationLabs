7beb2526e526f4d408f8dfaee87b6630e26f54d5
rajguptaH <rajkumar00999.rk@gmail.com>
2023-09-03 15:32:55 +0530

    error handling resolved

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Controllers/ErrorHandlerController.cs
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Controllers/ErrorHandlerController.cs
@@ -11,15 +11,7 @@ namespace TestingAndCalibrationLabs.Web.UI.Controllers
         public IActionResult Index(string message)
         {
             var statuscode = HttpContext.Response.StatusCode;
-            switch (statuscode)
-            {
-                case 401:
-                    return View("401");
-                case 404:
-                    return View("404");
-            }
             return View("Index",message);
         }
-        
     }
 }
