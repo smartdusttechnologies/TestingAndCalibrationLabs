960d1f45cfbc47b5e92c44a04bb43281100cb3c0
nileshmishraji <131526102+nileshmishraji@users.noreply.github.com>
2023-09-06 14:48:33 +0530

    update

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Mappers/MappingProfile.cs
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Mappers/MappingProfile.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using TestingAndCalibrationLabs.Business.Core.Model;
 using TestingAndCalibrationLabs.Web.UI.Models;
-using TestingAndCalibrationLabs.Business.Core.Model;
 
 namespace TestingAndCalibrationLabs.Web.UI.Mappers
 {
@@ -37,8 +36,6 @@ namespace TestingAndCalibrationLabs.Web.UI.Mappers
             CreateMap<WorkflowStageDTO,WorkflowStageModel>().ReverseMap();
             CreateMap<UiPageNavigationModel, UiPageNavigationDTO>().ReverseMap();
             CreateMap<OtpDTO, OtpModel>().ReverseMap();
-            //CreateMap<UserDTO,OtpDTO>().ReverseMap();
-
         }
     }
 }
