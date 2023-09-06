e2a669b7c8bed68b8b2aa8f63380ad68ef9b6309
nileshmishraji <131526102+nileshmishraji@users.noreply.github.com>
2023-09-06 14:38:14 +0530

    Update OtpDTO.cs

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Models/Common/OtpDTO.cs
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Models/Common/OtpDTO.cs
@@ -19,7 +19,7 @@ namespace TestingAndCalibrationLabs.Web.UI.Models
         /// </summary>
         public int userId { get; set; }
         /// <summary>
-        /// this used for otp
+        /// it used for otp
         /// </summary>
         public string OTP { get; set; }
         /// <summary>
