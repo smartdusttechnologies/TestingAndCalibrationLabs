2fa97bac0a63f5ae826c3495fb3e84989773b3a4
nileshmishraji <131526102+nileshmishraji@users.noreply.github.com>
2023-09-29 00:05:53 +0530

    updated table

---
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/UserOtp_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserOtp] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [UserId]      BIGINT        NULL,
+    [Otp]         VARCHAR (200) NULL,
+    [CreatedDate] DATETIME      NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
