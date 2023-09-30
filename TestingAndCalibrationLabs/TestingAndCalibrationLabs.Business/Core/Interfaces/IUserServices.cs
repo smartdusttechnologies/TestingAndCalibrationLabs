e96e342cac75a3db1beb9571de30649afb172c18
nileshmishraji <131526102+nileshmishraji@users.noreply.github.com>
2023-10-01 01:57:30 +0530

    update code

---
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Business/Core/Interfaces/IUserServices.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TestingAndCalibrationLabs.Business.Core.Model;
+
+namespace TestingAndCalibrationLabs.Business.Core.Interfaces
+{
+    public interface IUserService
+    {
+        /// <summary>
+        /// To Get all Records From User table.
+        /// </summary>
+        /// <returns></returns>
+        List<UserModel> Get();
+    }
+}
