f890dd0b2f2eef0d7189716738ff1f05dcd70eab
Apurwa11-894 <121474673+Apurwa11-894@users.noreply.github.com>
2023-10-19 17:58:32 +0530

    Commit

---
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.DB/dbo/Tables/SubPermissionModuleType.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SubPermissionModuleType] (
+    [Id]                     INT          IDENTITY (1, 1) NOT NULL,
+    [Name]                   VARCHAR (50) NOT NULL,
+    [PermissionModuleTypeId] INT          NOT NULL,
+    [IsDeleted]              BIT          CONSTRAINT [DF_SubPermissionModuleTypeId_IsDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_SubPermissionModuleTypeId] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_SubPermissionModuleTypeId_PermissionModuleType] FOREIGN KEY ([PermissionModuleTypeId]) REFERENCES [dbo].[PermissionModuleType] ([Id])
+);
+
---
+++ TestingAndCalibrationLabs/TestingAndCalibrationLabs.Web.UI/Common/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace TestingAndCalibrationLabs.Web.UI.Common
+{
+    /// <summary>
+    /// Handling Exceptions
+    /// </summary>
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        /// <summary>
+        /// This method get Called When Any Execption is Invoked
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+        /// <summary>
+        /// Handle the exception of this pipeline and redirect to ruled url with message
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            var message =  exception.Message;
+            switch (exception)
+            {
+                //We need to extend Cases like to handle accurate Exceptions
+                case UnauthorizedAccessException ex:
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    break;
+                default:
+                    context.Response.StatusCode = (int)StatusCodes.Status500InternalServerError;
+                    message = "Unable To Process Your Request  Please Contact To Admin";
+                    break;
+            }
+            context.Response.Redirect($"/ErrorPage/Index?message={message}");
+        }
+    }
+}
